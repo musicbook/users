576968085d171e77a16492c29f32b54b129b35c6
Jan Šubelj <jan.subelj010@gmail.com>
2018-01-04 21:31:37 +0100

    first

---
+++ src/main/java/com/fri/musicbook/UsersApp.java
@@ -0,0 +1,13 @@
+package com.fri.musicbook;
+
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+
+@RegisterService
+@ApplicationPath("v1")
+public class UsersApp extends Application {
+}
